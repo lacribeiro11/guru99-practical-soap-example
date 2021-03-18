60bfb4a389c9884b3cc04ecc9e3605ae5db126c9
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2021-03-18 19:59:42 +0100

    Add Server

---
+++ server/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Http;
+
+namespace server
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
---
+++ server/service.asmx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+
+namespace server
+{
+    [WebService(Name ="Calculator")]
+    public class service : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public int add(int a, int b)
+        {
+            return a + b; 
+        }
+        [WebMethod]
+        public int sub(int a, int b)
+        {
+            return a - b;
+        }
+        [WebMethod]
+        public int mul(int a, int b)
+        {
+            return a * b;
+        }
+        [WebMethod]
+        public int div(int a, int b)
+        {
+            return b == 0 ? 0 : a / b;
+        }
+    }
+}
