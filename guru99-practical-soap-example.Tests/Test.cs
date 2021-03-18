1202c113a2a343c94d2b0c83b503b198d7fdc356
Luis Ribeiro <luis.ribeiro@stud.fh-campuswien.ac.at>
2021-03-18 19:14:40 +0100

    Create the Web Server

---
+++ guru99-practical-soap-example.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace guru99practicalsoapexample.Tests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestCase()
+        {
+        }
+    }
+}
---
+++ webservice.asmx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web;
+using System.Web.Services;
+
+namespace guru99practicalsoapexample
+{
+    [WebService(Name ="My Webservice")]
+    public class webservice : System.Web.Services.WebService
+    {
+        [WebMethod]
+        public string home()
+        {
+            return "My Service";
+        }
+
+    }
+}
